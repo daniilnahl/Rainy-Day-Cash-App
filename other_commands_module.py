62e4dcf5c77bc5aa9b0fa542f4792f6b55940b7e
daniilnahl <daniil.nahl@protonmail.com>
2024-09-20 13:58:40 -0700

    v0.06.6
    Finished code in delete_transaction() in ocm.py module which is responsible for checking if the index of the transaction user wants to delete exists and if yes deletes the transaction.

--- other_commands_module.py
+++ other_commands_module.py
@@ -11,12 +11,18 @@ def delete_transaction(transactions):
     show_transactions('delete',transactions)
     print('Enter the number before the transaction that you would like to delete.')
     while True:
-        try: #keeps prompting user to enter a valid label for a transaction to be deleted
-            delete_transaction = int(input('Enter numbered label: '))
-            transaction_index = delete_transaction - 1
-            print(f"Succesfully deleted transaction {delete_transaction}. {transactions[transaction_index]}\n")
-            del transactions[transaction_index]
-            break
-        except ValueError:
-            print(f"You have entered an invalid label value. You can only enter an integer such as '1'")
-    #add a check for if a transaction exists, add a check if a user is entering a integer non decimal for the "labeled transaction"
+            #keeps prompting user to enter a valid label for a transaction to be deleted
+            delete_transaction = int(input('Enter numbered label: ')) 
+            #ADD A try here with except value error 
+
+
+            transaction_index = delete_transaction - 1 #declares the index of the transaction element that would be deleted
+            if transaction_index not in range(len(transactions)): #checks if the index of the transaction user wants to delete is in the range of the size of the transactions list 
+                print("The transaction you want to delete is out of bounds and doesn't exist")
+            else:#deletes the transaction element
+                print(f"Succesfully deleted transaction {delete_transaction}. {transactions[transaction_index]}\n")
+                del transactions[transaction_index]
+                break
+        
+            
+   
